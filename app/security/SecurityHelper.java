9f9528b60e6ddde36677d8d123a51623d37ab972
vineet kumar singh <vineet.singh@applift.com>
2016-03-12 15:17:33 +0530

    adding login sql

--- app/security/SecurityHelper.java
+++ app/security/SecurityHelper.java
@@ -9,15 +9,17 @@ import enum_package.HeaderKey;
 public class SecurityHelper {
 	public Map<HeaderKey, String> getAuthTokenFromRequest(Http.Context ctx) {
 		Map<HeaderKey, String> userHeaderCredential = new HashMap<HeaderKey, String>();
-		String[] authTokenHeaderValue = ctx.request().headers().get("X-SRP-TOKEN");
-		String[] userNameHeaderValue = ctx.request().headers().get("SRP-USER-NAME-TOKEN");
-		if(authTokenHeaderValue != null && authTokenHeaderValue.length == 1 &&
-				authTokenHeaderValue[0] != null && !authTokenHeaderValue[0].isEmpty())
-			userHeaderCredential.put(HeaderKey.AUTH_TOKEN, authTokenHeaderValue[0]);
+//		String[] authTokenHeaderValue = ctx.request().headers().get("X-SRP-TOKEN");
+//		String[] userNameHeaderValue = ctx.request().headers().get("SRP-USER-NAME-TOKEN");
+		String authTokenValue = ctx.session().get("X-SRP-TOKEN");
+		String userNameValue = ctx.session().get("SRP-USER-NAME-TOKEN");
 
-		if(userNameHeaderValue != null && userNameHeaderValue.length == 1 &&
-				userNameHeaderValue[0] != null && !userNameHeaderValue[0].isEmpty())
-			userHeaderCredential.put(HeaderKey.USER_NAME, userNameHeaderValue[0]);
+		if(authTokenValue != null && !authTokenValue.isEmpty())
+			userHeaderCredential.put(HeaderKey.AUTH_TOKEN, authTokenValue);
+
+		if(userNameValue != null && !userNameValue.isEmpty())
+			userHeaderCredential.put(HeaderKey.USER_NAME, userNameValue);
+
 		if(!userHeaderCredential.isEmpty() && userHeaderCredential.size() == 2)
 			return userHeaderCredential;
 
