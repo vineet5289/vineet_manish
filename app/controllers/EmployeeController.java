8341664364dc95652dafccfdf98292fc44383842
vineet kumar singh <vineet5289@gmail.com>
2016-07-31 02:00:53 +0530

    generating emp user name

--- app/controllers/EmployeeController.java
+++ app/controllers/EmployeeController.java
@@ -1,10 +1,35 @@
 package controllers;
 
+import enum_package.SessionKey;
 import play.data.Form;
 import play.mvc.Result;
 import views.forms.SchoolFormData;
 
 public class EmployeeController extends CustomController {
+
+	/*
+	 * validate valid user
+	 * validate user has access right or not
+	 * */
+	public Result preAddEmployeeRequest() {
+		String superUserName = session().get(SessionKey.SUPER_USER_NAME.name());
+		String superUserAuthKey = session().get(SessionKey.SUPER_AUTH_TOKEN.name());
+		String superUserSchoolId = session().get(SessionKey.SUPER_SCHOOL_ID.name());
+
+		return ok("");
+	}
+
+	/*
+	 * validate valid user
+	 * validate user has access right or not
+	 * */
+	public Result postAddEmployeeRequest() {
+		String superUserName = session().get(SessionKey.SUPER_USER_NAME.name());
+		String superUserAuthKey = session().get(SessionKey.SUPER_AUTH_TOKEN.name());
+		String superUserSchoolId = session().get(SessionKey.SUPER_SCHOOL_ID.name());
+		return ok("");
+	}
+
 	public Result preEmployeeRegistrationRequest() {
 		SchoolFormData studentData = new SchoolFormData();
 		Form<SchoolFormData> schoolForm = Form.form(SchoolFormData.class).fill(studentData);
