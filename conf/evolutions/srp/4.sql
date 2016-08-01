78378d4b21357ba6b3676cb1882e5025d261d7b5
vineet kumar singh <vineet5289@gmail.com>
2016-08-01 23:14:01 +0530

    adding school id as FK

--- conf/evolutions/srp/4.sql
+++ conf/evolutions/srp/4.sql
@@ -14,7 +14,12 @@ CREATE TABLE IF NOT EXISTS school_shift_info (
   shift_start_class_name varchar(10) NOT NULL,
   shift_end_class_name varchar(10) NOT NULL,
   shift_attendence_type enum('ONCE_DAY', 'TWICE_DAY', 'EVERY_PERIOD') DEFAULT 'REQUESTED' NOT NULL,
-  PRIMARY KEY (id)
+  school_id bigint(20) NOT NULL,
+  PRIMARY KEY (id),
+  KEY FK_school_shift_info_school_id (school_id),
+  CONSTRAINT FK_school_shift_info_school_id FOREIGN KEY (school_id) REFERENCES school (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 # --- !Downs
+
+drop table school_shift_info;
