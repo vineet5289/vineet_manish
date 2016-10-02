23a7b22e69d00fba7a80bb9b766f9338dd7dc5f7
Manyysh <manishyogesh11@gmail.com>
2016-10-03 01:57:05 +0530

    add roles

--- public/javascripts/js/school.js
+++ public/javascripts/js/school.js
@@ -1,3 +1,19 @@
+//addRole.scala.html js file
 $(document).ready(function(){
 $('.ui.dropdown').dropdown();
+
+$('.disableRoleBtn').click(function(e){
+   $(this).closest('tr').remove()
+})
+
+ $('.addNewRoleBtn').click(function(){
+     $('#addRoleModal').modal('show');
+    });
+
 });
+
+
+
+
+
+
