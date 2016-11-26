a40de0afebce9db96eef45aa6ba110aafce2fddf
vineet kumar singh <vineet5289@gmail.com>
2016-11-26 16:41:53 +0530

    insert employee into table

--- app/dao/employee/EmployesDAO.java
+++ app/dao/employee/EmployesDAO.java
@@ -11,11 +11,16 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.StringUtils;
+
 import play.db.Database;
 import play.db.NamedDatabase;
+import utils.EmployeeUtil;
+import utils.RandomGenerator;
 import views.forms.employee.AddEmployeeForm;
 import views.forms.employee.EmployeeDetailsForm;
 import dao.Tables;
+import enum_package.LoginState;
 
 public class EmployesDAO {
 
@@ -29,7 +34,9 @@ public class EmployesDAO {
     PreparedStatement loginPS = null;
     PreparedStatement empInsertPS = null;
     PreparedStatement empSelectPS = null;
+    ResultSet empSelectRS = null;
 
+    boolean executedSuccessfully = false;
     String loginQ =
         String.format("INSERT INTO %s (%s, %s, %s, %s, %s, %s) VALUES (?, ?, ?, ?, ?, ?);",
             Tables.Login.table, Tables.Login.userName, Tables.Login.password,
@@ -50,25 +57,72 @@ public class EmployesDAO {
             Tables.Employee.instituteId);
 
     String empCode = "";
+    String empUserName = "";
     try {
       connection = db.getConnection();
       connection.setAutoCommit(false);
-      if (addEmployeeDetails.getEmpCode() == null
-          || addEmployeeDetails.getEmpCode().trim().isEmpty()) {
+      empSelectPS = connection.prepareStatement(empSelectQ, ResultSet.TYPE_FORWARD_ONLY);
+      empInsertPS = connection.prepareStatement(empInsertQ);
+      loginPS = connection.prepareStatement(loginQ);
+      empSelectPS.setLong(1, instituteId);
 
-        // empCode =
+      int numberOfEmp = 0;
+      empSelectRS = empSelectPS.executeQuery();
+
+      if (empSelectRS.next()) {
+        numberOfEmp = empSelectRS.getInt(1);
       }
 
-      connection.commit();
+      if (addEmployeeDetails.isAutoGenerate()
+          || StringUtils.isBlank(addEmployeeDetails.getEmpCode())) {
+        empUserName = EmployeeUtil.getEmpUserName(instituteId, numberOfEmp);
+        empCode = empUserName;
+      } else {
+        empCode = addEmployeeDetails.getEmpCode().trim();
+        empUserName = EmployeeUtil.getEmpUserName(instituteId, empCode, numberOfEmp);
+      }
+
+      empInsertPS.setString(1, addEmployeeDetails.getEmpName().trim());
+      empInsertPS.setString(2, empUserName);
+      empInsertPS.setLong(3, instituteId);
+      empInsertPS.setString(4, addEmployeeDetails.getGender().trim().toUpperCase());
+      empInsertPS.setString(5, addEmployeeDetails.getEmpPhoneNumber().trim());
+      empInsertPS.setString(6, empCode);
+      empInsertPS.setString(7, addEmployeeDetails.getEmpEmail());
+      empInsertPS.setString(8, addEmployeeDetails.getJobTitle());
+      empInsertPS.setString(9, userName);
+      if (empInsertPS.executeUpdate() > 0) {
+        loginPS.setString(1, empUserName);
+        loginPS.setString(2, RandomGenerator.getBCryptPassword(empUserName));
+        loginPS.setString(3, LoginState.firststate.name());
+        loginPS.setString(4, addEmployeeDetails.getEmpName().trim());
+        loginPS.setString(5, addEmployeeDetails.getEmpEmail());
+        loginPS.setLong(6, instituteId);
+        if (loginPS.executeUpdate() > 0) {
+          connection.commit();
+          executedSuccessfully = true;
+        } else {
+          throw new Exception("Employee Registration failed");
+        }
+      } else {
+        throw new Exception("Employee Registration failed");
+      }
     } catch (Exception exception) {
       connection.rollback();
+      executedSuccessfully = false;
     } finally {
+      if (empSelectRS != null)
+        empSelectRS.close();
       if (loginPS != null)
+        loginPS.close();
+      if (empInsertPS != null)
+        loginPS.close();
+      if (empSelectPS != null)
         loginPS.close();
       if (connection != null)
         connection.close();
     }
-    return true;
+    return executedSuccessfully;
   }
 
   public List<EmployeeDetailsForm> getAllEmp(Long instituteId, boolean status) throws SQLException {
--- app/utils/EmployeeUtil.java
+++ app/utils/EmployeeUtil.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.util.Calendar;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,6 +12,9 @@ public class EmployeeUtil {
 
   private static Pattern jobTitlePattern;
   private static final String JOBTITLE_PATTERN = "^[a-zA-Z0-9 .]+$";
+
+  private static final int lengthOfSchoolId = 3;
+  private static final int lengthOfNumberOfEmp = 3;
 
   static {
     empCodePattern = Pattern.compile(EMPCODE_PATTERN, Pattern.CASE_INSENSITIVE);
@@ -33,5 +37,34 @@ public class EmployeeUtil {
 
     Matcher matcher = jobTitlePattern.matcher(jobTitle.trim());
     return matcher.matches();
+  }
+
+  public static String getEmpUserName(Long schoolId, String empId, long numberOfEmp) {
+      StringBuilder sb = new StringBuilder();
+      sb.append("E-");
+      sb.append(getFixedLengthString(schoolId, lengthOfSchoolId));
+      sb.append(empId.toUpperCase());
+      sb.append(getFixedLengthString(numberOfEmp, lengthOfNumberOfEmp));      
+      return sb.toString();
+  }
+
+  public static String getEmpUserName(Long schoolId, long numberOfEmp) {
+      int year = Calendar.getInstance().get(Calendar.YEAR);
+      StringBuilder sb = new StringBuilder();
+      sb.append("E-");
+      sb.append(getFixedLengthString(schoolId, lengthOfSchoolId));
+      sb.append(String.valueOf(year));
+      sb.append(getFixedLengthString(numberOfEmp, lengthOfNumberOfEmp));      
+      return sb.toString();
+  }
+
+  private static String getFixedLengthString(long number, int size) {
+      StringBuilder sb = new StringBuilder();
+      String numberAsString = String.valueOf(number);
+      while(sb.length() + numberAsString.length() < size) {
+          sb.append('0');
+      }
+      sb.append(number);
+      return sb.toString();
   }
 }
