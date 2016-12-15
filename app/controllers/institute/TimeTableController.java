081c8bc2e3d3f642a04993df31c1df952911a616
vineet kumar singh <vineet5289@gmail.com>
2016-12-15 22:36:22 +0530

    static timetable value for testing purpose

--- app/controllers/institute/TimeTableController.java
+++ app/controllers/institute/TimeTableController.java
@@ -1,11 +1,31 @@
 package controllers.institute;
 
-import play.mvc.Result;
 import controllers.CustomController;
+import play.data.Form;
+import play.data.FormFactory;
+import play.mvc.Result;
+import views.forms.institute.TimeTableForm;
+
+import javax.inject.Inject;
 
 public class TimeTableController extends CustomController {
-  public Result createTimeTable() {
+  @Inject
+  private FormFactory formFactory;
+
+  public Result preCreateTimeTable() {
+    long instituteId = 1l;
+    long classId = 1l;
+    long sectionId = 1l;
+    String classStartTime = "9:20 AM";
+    String classEndTime = "03:00 PM";
+    int duration = 40;
     return ok("");
+  }
+
+  public Result postCreateTimeTable() {
+    Form<TimeTableForm> timeTableFormForm = formFactory.form(TimeTableForm.class).bindFromRequest();
+    System.out.println("timeTableFormForm:" + timeTableFormForm);
+    return ok("timetable created");
   }
 
   public Result editTimeTable() {
